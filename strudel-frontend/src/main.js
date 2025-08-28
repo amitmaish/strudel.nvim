6bc24c87a7b2474c4409908c55cf236ecd71d2ea
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-08-28 11:49:29 -0700

    strudel web example

--- strudel-frontend/src/main.js
+++ strudel-frontend/src/main.js
@@ -1,13 +1,21 @@
-const app = document.querySelector("#app");
-
-let paragraph = document.createElement("p");
-paragraph.textContent = "hello world";
-
-let button = document.createElement("button");
-button.appendChild(document.createTextNode("button"));
-button.addEventListener("click", () => {
-  console.log("hello");
+import { initStrudel } from "@strudel/web";
+initStrudel({
+  prebake: () => samples("github:tidalcycles/dirt-samples"),
 });
 
-app.appendChild(paragraph);
-app.appendChild(button);
+const app = document.getElementById("app");
+app.innerHTML = `
+<button id="a">A</button>
+<button id="b">B</button>
+<button id="c">C</button>
+<button id="stop">stop</button>
+`;
+
+const click = (id, action) =>
+  document.getElementById(id).addEventListener("click", action);
+click("a", () => evaluate(`s('bd,jvbass(3,8)').jux(rev)`));
+click("b", () => evaluate(`s("bd*2,hh(3,4),jvbass(5,8,1)").jux(rev)`));
+click("c", () =>
+  evaluate(`s("bd*2,hh(3,4),jvbass:[0 4](5,8,1)").jux(rev).stack(s("~ sd"))`),
+);
+click("stop", () => evaluate("hush()"));
